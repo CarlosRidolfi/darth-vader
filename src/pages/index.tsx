c96eaf42f064b9eaff7438a53c75623a69a6a6e2
Carlos Ridolfi <carlos.a.ridolfi@gmail.com>
2023-07-13 07:00:34 -0300

    Making some changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,16 @@
 import Banner from '@/app/components/banner'
 import '../app/globals.css'
 import React from "react"
+import Head from 'next/head'
+import Favicon from '../assets/favicon.ico'
 
 export default function Home() {
   return (
     <main>
+      <Head>
+        <title>Darth Vader</title>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+      </Head>
       <Banner />
     </main>
   )
